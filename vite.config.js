bddd2989e778545a4fae35c21835dec3189bb88f
reshav401 <reshav401@gmail.com>
2023-04-10 16:52:37 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,22 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
-    sourcemap: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-        },
+  emptyOutDir: true,
+  base: '/menu/',
+  plugins: [react()],
+  define: {
+      'process.env': {},
+      __DEFINES__: {
+        API_BASE_URL: '52.221.216.231',
+        ENABLE_FEATURE_X: true,
+        ENABLE_FEATURE_Y: false,
       },
-    },
   },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    watch: false,
-  },
-});
+  // server:{
+  //   hmr:{
+  //     overlay:false
+  //   }
+  // }
+})
