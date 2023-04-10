13a40d4e769ff479c40347a5d7130a3d6b0f8276
reshav401 <reshav401@gmail.com>
2023-04-10 15:52:13 +0545

    .'

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  emptyOutDir: true ,
-  plugins: [react()],
+  base: '/menu/',
   define: {
     'process.env': {},
-    
     __DEFINES__: {
       API_BASE_URL: 'http://52.221.216.231',
       ENABLE_FEATURE_X: true,
       ENABLE_FEATURE_Y: false,
     },
   },
-  // optimizeDeps:{
-  //   exclude: ['@vite/client', '@vite/env'],
-  // },
-  base:'/menu/',
   server: {
     hmr: {
       protocol: 'ws',
-      port: 3000,
+      // port: 3000,
     },
   },
+  // optimizeDeps:{
+  //   exclude: ['@vite/client', '@vite/env'],
+  // },
+  emptyOutDir: true,
+  plugins: [react()],
 })
