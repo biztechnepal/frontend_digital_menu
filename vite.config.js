2bbcec0fd272b20239e1fa86661927d57a71912c
reshav401 <reshav401@gmail.com>
2023-04-10 16:58:15 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  emptyOutDir: true,
-  base: '/menu/',
+  emptyOutDir: true ,
   plugins: [react()],
-  define: {
-      'process.env': {},
-      __DEFINES__: {
-        API_BASE_URL: '52.221.216.231',
-        ENABLE_FEATURE_X: true,
-        ENABLE_FEATURE_Y: false,
-      },
+  define:{
+    'process.env': {}
   },
   // server:{
   //   hmr:{
