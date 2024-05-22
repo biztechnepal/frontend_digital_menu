c7eaa3cd2270712343d6fcbf9d7a48decbdbe2af
reshav401 <reshav401@gmail.com>
2024-05-22 08:19:08 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,15 @@ export default defineConfig({
   emptyOutDir: true,
   plugins: [react()],
   define: {
-    // __DEFINES__: {
-    //   API_URL: "https://qrmenu.gorkhab.com:878", // Define API_URL with a specific value
-    // },
     "process.env": {},
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://qrmenu.gorkhab.com:877",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
   // server: {
-  //   hmr: {
-  //     overlay: false,
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://qrmenu.gorkhab.com:877",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
   //   },
   // },
 });
